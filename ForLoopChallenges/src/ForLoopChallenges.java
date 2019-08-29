c6e1dd7e3763ba4d7aaad0985d323f2322c6467e
lauren.sciullo <lauren.sciullo@MU-C1-06.mullen.pvt.k12.co.us>
2019-08-29 12:22:50 -0600

    foor loop challenges!!

---
+++ ForLoopChallenges/src/ForLoopChallenges.java
@@ -0,0 +1,78 @@
+
+public class ForLoopChallenges
+	{
+
+		public static void main(String[] args)
+			{
+				//printDonutsLove();
+				//ascendingToDescending();
+				multiplesOfFive();
+				//totalOfMultiples();
+				sumNumbersInRange();
+				drawRectangleAsterisks();
+				printUsingTens();
+
+			}
+
+		private static void printDonutsLove()
+			{
+				for (int i = 0; i < 5; i++)
+					{
+				System.out.println("I love doughnuts!");
+					}
+			}
+
+		private static void ascendingToDescending()
+			{
+					
+				for (int i = 1; i <= 5;i++)
+					{
+				System.out.println("Ascending: " + i);
+					}
+				for (int i = 5; i >=1; i--)
+					{
+						System.out.println("\nDescending: " + i);
+					}
+			}
+
+		private static void multiplesOfFive()
+			{
+				for (int i = 5; i<=100; i++ )
+					{
+						if (i%5 == 0)
+							{
+								System.out.println(i);
+							}
+					}
+			}
+
+//		private static void totalOfMultiples()
+//			{
+//				int sum = 0;
+//				for (int number)
+//					{
+//						sum+=number;
+//						System.out.println();
+//					}
+//				System.out.println("The sum is " + sum);
+//			}
+
+		private static void sumNumbersInRange()
+			{
+				
+				
+			}
+
+		private static void drawRectangleAsterisks()
+			{
+				
+				
+			}
+
+		private static void printUsingTens()
+			{
+				
+				
+			}
+
+	}
